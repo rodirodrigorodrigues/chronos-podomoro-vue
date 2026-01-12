a101a332b365e381bcec829249f40682a5626a52
rodirodrigorodrigues <mspdhuwqheuwms@gmail.com.bad>
2026-01-12 15:29:10 -0300

    feat: add creation of task and getNextCycle function

--- src/components/MainForm/MainForm.vue
+++ src/components/MainForm/MainForm.vue
@@ -4,18 +4,45 @@ import { PlayCircleIcon } from "lucide-vue-next";
 import Cycles from "../Cycles/index";
 import DefaultButton from "../DefaultButton/index";
 import DefaultInput from "../DefaultInput/index";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskStore } from "../../store/taskStore";
+
+const taskStore = useTaskStore();
 
 const taskName = ref<string>("");
 
 function handleCreateNewTask(e: Event) {
-  console.log("Value of input:", taskName.value);
+  const formattedTaskName = taskName.value.trim();
+  if (!formattedTaskName) {
+    alert("Please enter a valid task name.");
+    return;
+  }
+
+  const newTask: TaskModel = {
+    id: Date.now().toString(),
+    name: taskName.value,
+    startDate: Date.now(),
+    completeDate: null,
+    interruptDate: null,
+    duration: 1,
+    type: "workTime",
+  };
+
+  // Atualiza o estado do Pinia
+  taskStore.createTask(newTask);
 }
 </script>
 
 <template>
   <form class="form" @submit.prevent="handleCreateNewTask">
     <div class="formRow">
-      <DefaultInput labelText="Label" id="meuInput" type="text" placeholder="Digite algo" v-model="taskName"/>
+      <DefaultInput
+        labelText="Label"
+        id="meuInput"
+        type="text"
+        placeholder="Digite algo"
+        v-model="taskName"
+      />
     </div>
     <div className="formRow">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
--- src/store/taskStore.ts
+++ src/store/taskStore.ts
@@ -1,19 +1,30 @@
-import { defineStore } from 'pinia';
-import type { TaskStateModel } from '../models/TaskStateModel';
+import { defineStore } from "pinia";
+import type { TaskStateModel } from "../models/TaskStateModel";
+import type { TaskModel } from "../models/TaskModel";
+import { getNextCycle } from "../utils/getNextCycle";
 
 const initialState: TaskStateModel = {
-    tasks: [],
-    secondsRemaining: 0,
-    formattedSecondsRemaining: "00:00",
-    activeTask: null,
-    currentCycle: 0,
-    config: {
-        workTime: 25,
-        shortBreakTime: 5,
-        longBreakTime: 15,
-    },
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
 };
 
-export const useTaskStore = defineStore('taskStore', {
-    state: (): TaskStateModel => ({ ...initialState }),
-})
+export const useTaskStore = defineStore("taskStore", {
+  state: (): TaskStateModel => ({ ...initialState }),
+  actions: {
+    createTask(task: TaskModel) {
+      this.tasks.push(task);
+      this.activeTask = task;
+      this.secondsRemaining = task.duration * 60;
+      this.formattedSecondsRemaining = "00:00";
+      this.currentCycle = getNextCycle(this.currentCycle);
+    },
+  },
+});
